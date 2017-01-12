f40bbc35f3a184d74b66a7bc6a5530d45bc71302
Gabor Fetter <fetter.gabor@infoframe.hu>
2017-01-12 13:39:57 +0100

    Fixed android stuff

---
+++ Weather_Droid/Bootstrap/DownloadCachePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Weather_Droid.Bootstrap
+{
+    public class DownloadCachePluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.DownloadCache.PluginLoader>
+    {
+    }
+}
